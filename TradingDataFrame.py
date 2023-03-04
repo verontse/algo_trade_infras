76a8b4e80947de1e214ba1f511d63df28a7fec0a
verontse <114328843+verontse@users.noreply.github.com>
2023-03-04 13:17:47 +0800

    Add files via upload

--- TradingDataFrame.py
+++ TradingDataFrame.py
@@ -3,21 +3,14 @@ import numpy as np
 from datetime import datetime
 
 class TradingDataFrame():
-    def __init__(self, df, time_col, price_criteria_pair, time_unit=None):
+    def __init__(self, df, time_col, price_criteria_pair=None, price_cols=[], time_unit=None):
         
         self.time_col = time_col
         self.price_criteria_pair = price_criteria_pair
-        
-        # Remove unnecessary columns
-        price_cols, criteria_cols = [], []
-        for price_col, criteria_col in price_criteria_pair.items():
-            price_cols.append(price_col)
-            criteria_cols.append(criteria_col)
+        self.price_cols = price_cols
 
-        criteria_cols = list(set(criteria_cols))
-        
-        cols = [time_col, *price_cols, *criteria_cols]
-        df = df[cols].copy()
+        if len(price_cols) == 0:
+            self.price_cols = [price for price in price_criteria_pair.keys()]
 
         # Convert price cols to numbers & add pct_change
         for col in price_cols:
