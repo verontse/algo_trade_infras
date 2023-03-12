dd62896567cdf2f6e13c3f42b1d7754e46477fcf
verontse <114328843+verontse@users.noreply.github.com>
2023-03-12 13:37:54 +0800

    Add files via upload
    - Optimized codes

--- strategy.py
+++ strategy.py
@@ -10,8 +10,6 @@ import pickle
 class Strategy():
     def __init__(self, name=None):
         self.name = name
-        self.price_criteria_pair = None
-        self.price_position_pair = None
         self.df = None
 
     def _calculate_maxdd(self, df):
@@ -26,7 +24,7 @@ class Strategy():
         self.short_entry_sign, self.short_entry_threshold = short_entry
         self.short_exit_sign, self.short_exit_threshold = short_exit
 
-    def apply_strategy(self, tdf):
+    def _apply_strategy(self, tdf):
         
         # Position long df
         if self.long_entry_sign == '>':
@@ -69,42 +67,55 @@ class Strategy():
         df_position_short.fillna(0, inplace=True)
 
         # Save position df
-        df_position_long.columns = tdf.returns.columns + '_pos_long'
-        df_position_short.columns = tdf.returns.columns + '_pos_short'
+        df_position_long.columns = tdf.log_returns.columns + '_pos_long'
+        df_position_short.columns = tdf.log_returns.columns + '_pos_short'
 
         self.position_long = df_position_long
         self.position_short = df_position_short
         
         print('Strategy applied!')
 
-    def backtest(self, tdf, trading_fee=0, check_long=False, check_short=False, check_overall=True, plot=True):
+    def backtest(self, tdf, trading_fee=0, check_long=False, check_short=False, check_overall=True, plot_returns='log'):
+        
+        # Apply strategy
+        self._apply_strategy(tdf)
 
         # Shift daily returns column up 1 row to calculate P&L
-        returns_next_day = tdf.returns.shift(-1).copy()
-        returns_next_day.columns = tdf.returns.columns + '_next_day'
+        returns_next_day = tdf.log_returns.shift(-1).copy()
+        returns_next_day.columns = tdf.log_returns.columns + '_next_day'
 
         # Merge returns and positions together
-        self.df = pd.merge(tdf.returns, returns_next_day, left_index=True, right_index=True)
+        self.df = pd.merge(tdf.log_returns, returns_next_day, left_index=True, right_index=True)
         self.df = pd.merge(self.df, self.position_long, left_index=True, right_index=True)
         self.df = pd.merge(self.df, self.position_short, left_index=True, right_index=True)
 
-        # Add P&L columns 
+        # Add trading fees
+        self.df['no_of_trades'] = abs(self.position_long.diff().values).sum(axis=1) + abs(self.position_short.diff().values).sum(axis=1)
+        self.df['trading fees'] = trading_fee * self.df['no_of_trades']
+
+        # Add P&L columns
         self.df['daily p&l'] = (returns_next_day.values * (self.position_long.values - self.position_short.values)).sum(axis=1)
+        self.df['daily p&l after fees'] = self.df['daily p&l'] - self.df['trading fees']
+
         self.df = self.df.iloc[1:-1,:]
-        self.df['cum p&l'] = (1 + self.df['daily p&l']).cumprod() - 1
+        self.df['cum p&l_log'] = self.df['daily p&l after fees'].cumsum()
+        self.df['cum p&l_simple'] = np.exp(self.df['cum p&l_log']) - 1
 
         # Other trade metrics
         self.no_of_trades = int((abs(self.position_long.diff()).sum()//2).sum() + (abs(self.position_short.diff()).sum()//2).sum())
-        self.sharpe_ratio = np.sqrt(len(self.df)) * np.mean(self.df['daily p&l']) / np.std(self.df['daily p&l'])
+        self.sharpe_ratio = np.sqrt(len(self.df)) * np.mean(self.df['daily p&l after fees']) / np.std(self.df['daily p&l after fees'])
         self.max_dd = self._calculate_maxdd(self.df)
 
         print(f'# of trades: {self.no_of_trades}')
         print(f'Sharpe ratio: {round(self.sharpe_ratio,2)}')
-        print(f'Max Drawdown: {round(self.max_dd * 100,2)}%')
+        #print(f'Max Drawdown: {round(self.max_dd * 100,2)}%')
 
-        if plot:
-            plt.xticks(rotation=45)
-            sn.lineplot(self.df['cum p&l'])
+        plt.xticks(rotation=45)
+
+        if plot_returns == 'simple':
+            sn.lineplot(self.df['cum p&l_simple'])
+        elif plot_returns == 'log':
+            sn.lineplot(self.df['cum p&l_log'])
 
     def save(self):
         with open(f'.\saved_strategy\{self.name}.pickle', 'wb') as file:
